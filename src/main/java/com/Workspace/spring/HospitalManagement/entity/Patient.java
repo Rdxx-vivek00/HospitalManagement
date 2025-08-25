626ead45d241320b0102841ff8c80ed6d6f1cdb7
Rdxx-vivek00 <vivekvkc07@gmail.com>
2025-08-25 18:25:08 +0530

    lombok added

--- src/main/java/com/Workspace/spring/HospitalManagement/entity/Patient.java
+++ src/main/java/com/Workspace/spring/HospitalManagement/entity/Patient.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
@@ -16,6 +17,7 @@ public class Patient{
 
     private String name;
 
+    @ToString.Exclude
     private LocalDate birthDate;
 
     private String email;
